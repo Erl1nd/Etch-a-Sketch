adff4b68085d90b34a3e1047181e80c2879376f3
Erlind <erlind.uxdesigner@gmail.com>
2023-05-02 12:19:11 +0800

    Added rainbow pen functionality

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let size = 100;
+let size = 16;
 let container = document.querySelector(".container");
 let div;
 let row
@@ -18,9 +18,42 @@ for (let i = 0; i < size; i++) {
 
 
 let divGrid = document.querySelectorAll(".div-grid");
+let rainbowPen = document.querySelector(".rainbow-pen");
+let clearCanvas = document.querySelector(".clear-canvas");
+let eraser = document.querySelector(".eraser");
+
+// draw 
 divGrid.forEach(element => {
     element.addEventListener("mouseover", function() {
         this.classList.add("black");
     })
 });
 
+
+// rainbow pen
+rainbowPen.addEventListener("click", function() {
+    rainbowPen.classList.toggle("toggled");
+
+    if (rainbowPen.classList.contains("toggled")) {
+        divGrid.forEach(element => {
+            element.addEventListener("mouseover", function() {
+                this.classList.add("rainbow");
+            })
+        });
+    } else {
+        divGrid.forEach(element => {
+            element.addEventListener("mouseover", function() {
+                this.classList.remove("rainbow");
+            })
+        });
+    }
+})
+
+
+// clear canvas
+clearCanvas.addEventListener("click", function(){
+    divGrid.forEach(element => {
+        element.classList.remove("black");
+        element.classList.remove("rainbow");
+    });
+})
