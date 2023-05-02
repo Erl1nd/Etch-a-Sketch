22eaeb2a19df5c180370618ee5d7018ef6ddc778
Erlind <erlind.uxdesigner@gmail.com>
2023-05-02 10:50:10 +0800

    Fixed grid system

--- js/script.js
+++ js/script.js
@@ -5,12 +5,12 @@ let row
 
 
 for (let i = 0; i < size; i++) {
-    row = document.createElement(`row-${i}`);
+    row = document.createElement(`row`);
     row.classList.add("row")
     container.appendChild(row);
 
     for (let j = 0; j < size; j++) {
-        div = document.createElement(`div-${j}`);
+        div = document.createElement(`div`);
         div.classList.add("div-grid");
         row.appendChild(div);
     }
