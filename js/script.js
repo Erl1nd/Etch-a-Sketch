90ae91a356fe581aa3aff77734dd54db946cf255
Erlind <erlind.uxdesigner@gmail.com>
2023-05-02 10:33:19 +0800

    Added auto generate grid system

---
+++ js/script.js
@@ -0,0 +1,17 @@
+let size = 16;
+let container = document.querySelector(".container");
+let div;
+let row
+
+
+for (let i = 0; i < size; i++) {
+    row = document.createElement(`row-${i}`);
+    row.classList.add("row")
+    container.appendChild(row);
+
+    for (let j = 0; j < size; j++) {
+        div = document.createElement(`div-${j}`);
+        div.classList.add("div-grid");
+        row.appendChild(div);
+    }
+}
