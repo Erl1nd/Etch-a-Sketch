40b570b5ecd28bd079008e30f1a2050d1e13f213
Erlind <erlind.uxdesigner@gmail.com>
2023-05-04 14:14:26 +0800

    remove the eventListener createNewCanvas fixed code, added functions

--- js/script.js
+++ js/script.js
@@ -1,111 +1,86 @@
-let container = document.querySelector(".container");
-let slider = document.getElementById("slider");
-let createNewGird = document.querySelector(".create-new-grid");
+const defaultSize = 35;
+const container = document.querySelector(".container");
+const slider = document.getElementById("slider");
+const createNewCanvas = document.querySelector(".create-new-canvas")
+const penColor = document.getElementById("pen-color");
+const canvasColor = document.getElementById("canvas-color");
+const toggleGrid = document.querySelector(".toggle-grid");
+const toggleEraser = document.querySelector(".toggle-eraser");  
+const toggleRainbowPen = document.querySelector(".toggle-rainbow-pen"); 
+const clearCanvas = document.querySelector(".clear-canvas");
+let eraserOn = false;
+let rainbowPenOn = false;
 
 
-container.addEventListener("mouseenter", function() {
-    if ((container.hasChildNodes(".row")) === false) {
-        let h1;
-        h1 = document.createElement("h1");
-        h1.textContent = "Please Create a New Canvas"
-        h1.style.color = "black"
-        h1.classList.add("warning")
-        container.appendChild(h1);
-    } 
+populateBoard(35);
+if (eraserOn == false && rainbowPenOn == false) {
+    document.querySelectorAll(".div-grid").forEach(element => {
+        element.addEventListener("mouseover", function() {
+            this.style.backgroundColor = `${penColor.value}`;
+        })
+    });
+}
+
+// CHANGE CANVAS COLOR
+canvasColor.addEventListener("change", function(){
+    container.style.backgroundColor = canvasColor.value;
 })
 
-createNewGird.addEventListener("click", function(){
-    // SETUP GRID
-    deletePreviousGrid();
-    let amount = slider.value;
-    makeGrid(amount);
-
-    // DRAW USING PEN COLOR
-    let penColor = document.getElementById("fav-color");
-    drawUsingPen(penColor.value);
-
-    // CHANGE CANVAS COLOR
-    let canvasColor = document.getElementById("canvas-color");
-    canvasColor.addEventListener("change", function(){
-        container.style.backgroundColor = canvasColor.value;
-    })
-        
-    // TOGGLE GRID
-    let toggleGrid = document.querySelector(".toggle-grid");
-    let divGrid = document.querySelectorAll(".div-grid");
-    toggleGrid.addEventListener("click", function(){
-        if (toggleGrid.classList.contains("button-on")) {
-            toggleGrid.classList.remove("button-on")
-        } else {
-            toggleGrid.classList.add("button-on")
-        }
-        divGrid.forEach(element => {
-            element.classList.toggle("div-grid-border");
-        });
-    })
-
-    // TOGGLE ERASER
-    let toggleEraser = document.querySelector(".toggle-eraser");   
-    toggleEraser.addEventListener("click", function(){
-        if (toggleEraser.classList.contains("button-on")) {
-            toggleEraser.classList.remove("button-on")
-        } else {
-            toggleEraser.classList.add("button-on")
-        }
-        toggleRainbowPen.classList.remove("button-on");
-        if(toggleEraser.classList.contains("button-on")) {
-            divGrid.forEach(element => {
-                element.addEventListener("mouseover", function() {
-                    this.style.backgroundColor = "transparent";
-                })
-            });
-        } else {
-            divGrid.forEach(element => {
-                element.addEventListener("mouseover", function() {
-                    this.style.backgroundColor = `${penColor.value}`;
-                })
-            });
-        }
-    })
-
-    // TOGGLE RAINBOW PEN
-    let toggleRainbowPen = document.querySelector(".toggle-rainbow-pen");
-    toggleRainbowPen.addEventListener("click", function(){
-        toggleRainbowPen.classList.toggle("button-on");
-        toggleEraser.classList.remove("button-on");
-        if(toggleRainbowPen.classList.contains("button-on")) {
-            divGrid.forEach(element => {
-                let randomColor = Math.floor(Math.random()*16777215).toString(16);
-                element.addEventListener("mouseover", function() {
-                    this.style.backgroundColor = "#" + randomColor;
-                })
-            });
-        }  else {
-            divGrid.forEach(element => {
-                element.addEventListener("mouseover", function() {
-                    this.style.backgroundColor = `${penColor.value}`;
-                })
-            });
-        }
-    })
-
-    // CLEAR CANVAS
-    let clearCanvas = document.querySelector(".clear-canvas");
-    clearCanvas.addEventListener("click", function(){
-        divGrid.forEach(element => {
-            element.style.backgroundColor = "transparent";
-        });
-        
-    })
+// TOGGLE GRID
+toggleGrid.addEventListener("click", function (){
+    toggleGrid.classList.add("button-on")
+    document.querySelectorAll(".div-grid").forEach(element => {
+        element.classList.toggle("div-grid-border");
+    });
 })
 
-function deletePreviousGrid() {
-    let warning = document.querySelectorAll("h1");
+// ERASER
+toggleEraser.addEventListener("click", function(){
+    eraserOn = !eraserOn;
+    toggleEraser.classList.toggle("button-on");
+    if(eraserOn == true) {
+        erase();
+        rainbowPenOn = false;
+        if(toggleRainbowPen.classList.contains("button-on") == true){
+            toggleRainbowPen.classList.remove("button-on")
+        }
+    } else {
+        document.querySelectorAll(".div-grid").forEach(element => {
+            element.addEventListener("mouseover", function() {
+                this.style.backgroundColor = `${penColor.value}`;
+            })
+        });
+    }
+})
+
+// RAINBOWPEN
+toggleRainbowPen.addEventListener("click", function(){
+    rainbowPenOn = !rainbowPenOn;
+    toggleRainbowPen.classList.toggle("button-on");
+    if(rainbowPenOn == true) {
+        useRainbowPen();
+        eraserOn = false;
+        if(toggleEraser.classList.contains("button-on") == true){
+            toggleEraser.classList.remove("button-on");
+        }
+    } else {
+        document.querySelectorAll(".div-grid").forEach(element => {
+            element.addEventListener("mouseover", function() {
+                this.style.backgroundColor = `${penColor.value}`;
+            })
+        });
+    }
+})
+
+// CLEAR CANVAS
+clearCanvas.addEventListener("click", function(){
+    clear();
+})
+
+
+function deletePreviousCanvas() {
     let squares = document.querySelectorAll(".row")
     if (container.hasChildNodes()) {
-        warning.forEach(div => {
-            div.remove();
-        });
         squares.forEach(div => {
             div.remove();
         });
@@ -114,14 +89,14 @@ function deletePreviousGrid() {
     }
 }
 
-function makeGrid(sliderValue) {
-
-    for (let i = 0; i < sliderValue; i++) {
+function populateBoard(size) {
+    console.log(size);
+    let amount = size;
+    for (let i = 0; i < amount; i++) {
         let row = document.createElement(`row`);
         row.classList.add("row")
         container.appendChild(row);
-    
-        for (let j = 0; j < sliderValue; j++) {
+        for (let j = 0; j < amount; j++) {
             let div = document.createElement(`div`);
             div.classList.add("div-grid");
             row.appendChild(div);
@@ -129,12 +104,40 @@ function makeGrid(sliderValue) {
     } 
 }
 
-function drawUsingPen(penColor) {
-    let divGrid = document.querySelectorAll(".div-grid");
-    divGrid.forEach(element => {
+function erase() {
+    document.querySelectorAll(".div-grid").forEach(div => {
+        div.addEventListener("mouseover", function() {
+            this.style.backgroundColor = "transparent";
+        })
+    });  
+}
+
+function useRainbowPen() {
+    document.querySelectorAll(".div-grid").forEach(element => {
+        let randomColor = Math.floor(Math.random()*16777215).toString(16);
         element.addEventListener("mouseover", function() {
-            this.style.backgroundColor = `${penColor}`;
+            this.style.backgroundColor = "#" + randomColor;
         })
     });
 }
+
+function clear() {
+    let divGrid = document.querySelectorAll(".div-grid");
+    divGrid.forEach(element => {
+        element.style.backgroundColor = "transparent";
+    });
+}
+
+
+
+// function deletePreviousCanvas() {
+//     let squares = document.querySelectorAll(".row")
+//     if (container.hasChildNodes()) {
+//         squares.forEach(div => {
+//             div.remove();
+//         });
+//     } else {
+//         console.log("no previous squares");
+//     }
+// }
 
